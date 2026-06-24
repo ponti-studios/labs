eed77388a4bb1a763f0d500e8af927b2e1fac025
Charles Ponti <cj@ponti.io>
2026-06-23 21:44:36 -0700

    migrate seed-tfl to typescript

--- tools/scripts/populate-tfl.ts
+++
@@ -1,8 +0,0 @@
-import { populateTflCameras as populateTflCamerasFromDb } from "../../packages/db/src/loaders/tfl";
-
-populateTflCamerasFromDb().catch((error) => {
-  console.error("Population failed:", error);
-  process.exit(1);
-});
-
-export { populateTflCamerasFromDb as populateTflCameras };
